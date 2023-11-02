8c9b732af38596ade17f4bd68541d2236005f903
Reshna Raghu Chalil <84814192+rchalil@users.noreply.github.com>
2023-11-02 11:24:49 -0400

    Add files via upload

---
+++ Test Suites/Reshna - PMI Suite2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Reshna - PMI Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7b95d93f-739f-489d-ade8-9df49e054d56</testSuiteGuid>
+   <testCaseLink>
+      <guid>213e9d66-b83f-4b68-aa1e-31695e7518b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PMI - Verify the spanish language in PMI</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
